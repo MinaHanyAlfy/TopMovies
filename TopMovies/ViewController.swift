69d97ea5973b73a7235c03283e69f900ddd9962b
Mina Hany <10minahany10@gmail.com>
2026-05-24 17:10:23 +0300

    - Remove unnecessary code.

--- TopMovies/ViewController.swift
+++ TopMovies/ViewController.swift
@@ -8,15 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let network = NetworkClient()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let repo = MoviesRepositoryImpl(networkClient: network)
-        Task {
-            try await repo.fetchNowPlaying(page: 1)
-        }
+
     }
 
 
